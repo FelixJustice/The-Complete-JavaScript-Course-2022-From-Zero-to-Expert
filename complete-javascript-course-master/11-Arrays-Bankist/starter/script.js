2976b9657ce1a6272f9b9c615c79dfbb17fdacf7
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-04-23 13:35:40 +0300

    forEach Map and Set

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -153,3 +153,15 @@ movements.forEach(function (mov, i, arr) {
 // 2: function(400)
 // ...
 */
+
+// Map forEach
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set forEach
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
