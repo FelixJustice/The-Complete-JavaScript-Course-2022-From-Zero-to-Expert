a58d4daa39ad518eb843d9f8d855172f3ba25fcb
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-05-01 14:33:27 +0300

    Working with Arrays Challnege #2

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -300,6 +300,7 @@ const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
 */
 
+/*
 // ---------array.reduce() METHOD---------------
 console.log(movements);
 
@@ -322,3 +323,52 @@ const max = movements.reduce(
   movements[0]
 );
 console.log(max);
+*/
+
+/*
+Coding Challenge #2
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Your tasks:
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4
+
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+
+4. Run the function for both test datasets
+
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+
+
+const calcAverageHumanAge = function (dogAges) {
+  const humanAgesOfAdultDogs = dogAges
+    .map(dogAge => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
+    .filter(humanAge => humanAge >= 18);
+
+  // const averageHumanAgeOfAdultDogs =
+  //   humanAgesOfAdultDogs.reduce(
+  //     (acc, youngAdultAge) => (acc += youngAdultAge),
+  //     0
+  //   ) / humanAgesOfAdultDogs.length;
+
+  const averageHumanAgeOfAdultDogs = humanAgesOfAdultDogs.reduce(
+    (acc, youngAdultAge) =>
+      (acc += youngAdultAge / humanAgesOfAdultDogs.length),
+    0
+  );
+
+  return averageHumanAgeOfAdultDogs;
+};
+
+const average1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const average2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(average1, average2);
+*/
