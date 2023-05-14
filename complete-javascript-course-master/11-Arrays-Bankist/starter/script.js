3e47798d3c1a80caaae9c3456e12f5682eba6324
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-05-14 12:28:01 +0300

    Array.from()

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -603,7 +603,9 @@ const overallBalance2 = accounts
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(overallBalance2);
-*/
+
+
+//------------sort() method Array--------
 
 // Strings
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
@@ -633,3 +635,39 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+
+//-----------Array.from() how to create programatically an a array-------------
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+//console.log(x.map(() => 5)); // <--- does not work on this [ empty x 7]
+
+x.fill(1); // <-------- Works because it mutates.
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function (e) {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
