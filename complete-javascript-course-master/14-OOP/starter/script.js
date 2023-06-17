afb5c7dd9097a5ff6e009ab3e8892b233bff2af4
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-06-17 17:14:13 +0300

    Static in class

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /*
+ */
 // Constructor function
 const Person = function (firstName, birthYear) {
   //   console.log(this);
@@ -31,6 +32,13 @@ const jay = 'Jay';
 
 console.log(jonas instanceof Person);
 console.log(jay instanceof Person);
+
+Person.hey = function () {
+  console.log('Hey there!!');
+  console.log(this);
+};
+
+Person.hey();
 
 // Prototypes
 console.log(Person.prototype);
@@ -78,7 +86,6 @@ console.log(arr.unique());
 
 const h1 = document.querySelector('h1');
 console.dir(x => x + 1);
-*/
 
 // class expression
 // const PersonCl = class {};
@@ -90,6 +97,7 @@ class PersonCl {
     this.birthYear = birthYear;
   }
 
+  // Instance methods
   // Methods will be added to .prototype property
   calcAge() {
     console.log(2037 - this.birthYear);
@@ -113,6 +121,12 @@ class PersonCl {
   get fullName() {
     return this._fullName;
   }
+
+  // Static method
+  static hey() {
+    console.log('Hey there!!');
+    console.log(this);
+  }
 }
 
 const jessica = new PersonCl('Jessica Davis', 1996);
@@ -132,6 +146,8 @@ jessica.greet();
 // 3. Classes are executed in strict mode
 
 const walter = new PersonCl('Walter White', 1965);
+
+PersonCl.hey();
 
 const account = {
   owner: 'jonas',
