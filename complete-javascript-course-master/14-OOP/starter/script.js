f4adf2734be4f6b1d01e2efc8e026c6890f1ec3e
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-06-21 13:33:52 +0300

    Private and public fields and methods

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -327,34 +327,45 @@ jay.introduce();
 jay.calcAge();
 */
 
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also static version)
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin;
-    // protected property
-    this._movements = [];
+    this.#pin = pin;
 
-    this.locale = navigator.language;
+    // protected property convention style
+    // this._movements = [];
+
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account ${owner}`);
   }
 
+  // 3) Public methods
+
   // Public interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdrawal(val) {
     this.deposit(-val);
-  }
-
-  _approveLoan(val) {
-    return true;
   }
 
   requestLoan(val) {
@@ -362,6 +373,16 @@ class Account {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  //#approveLoan(val) {
+  _approveLoan(val) {
+    return true;
   }
 }
 
@@ -372,8 +393,14 @@ const acc1 = new Account('Jonas', 'EUR', 1111, []);
 acc1.deposit(250);
 acc1.withdrawal(140);
 acc1.requestLoan(1000);
-acc1._approveLoan(1000);
+// acc1.#approveLoan(1000);
 console.log(acc1.getMovements());
 
 console.log(acc1);
-console.log(acc1.pin);
+// console.log(acc1._pin);
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+
+Account.helper();
