39fc986ec4b6c052124acdd733b0b830a763686e
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-06-30 17:10:54 +0300

    CommonJS modules examples

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -43,7 +43,10 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
-*/
+
+
+
+// Old way of doing modules
 
 const ShoppingCart2 = (function () {
   const cart = [];
@@ -73,3 +76,15 @@ ShoppingCart2.addToCart('apple', 4);
 ShoppingCart2.addToCart('pizza', 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
+*/
+
+// CommonJS modules
+// Export
+// export.addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart.
+//     (shipping cost is ${shippingCost})`);
+//   };
+
+// Import
+// const { addToCart } = require('./shoppingCart.js')
