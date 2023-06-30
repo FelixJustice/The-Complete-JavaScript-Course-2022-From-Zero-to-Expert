d11d7cc5dc28879614564fe9137fb8186e9027c0
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-06-30 16:40:36 +0300

    Old way of doing modules

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -3,6 +3,7 @@
 // console.log(price, tq);
 // addToCart('bread', 5);
 
+/*
 console.log('Importing module');
 
 // import * as ShoppingCart from './shoppingCart.js';
@@ -18,6 +19,8 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+
+// Top level await
 
 // console.log('Start fetching');
 // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -40,3 +43,33 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart.
+    (shipping cost is ${shippingCost})`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier.`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
