ad16692b88bf850ef8447a97330e2dc40c109376
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-07-09 14:18:32 +0300

    Search implemented

--- complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -1,19 +1,20 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+// Comes from parcel!!
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
+    // console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -28,7 +29,28 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    console.log(resultsView);
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    console.log(query);
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Redner results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
