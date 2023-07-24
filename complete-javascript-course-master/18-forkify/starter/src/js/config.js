3ef9bf1be3944ee1c59df7c81c8613992d3287cc
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-07-24 08:48:46 +0300

    add new recipe implementation

--- complete-javascript-course-master/18-forkify/starter/src/js/config.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'f3b5e273-bf40-4043-b18e-6d38c22ecc2b';
+export const MODAL_CLOSE_SEC = 2.5;
